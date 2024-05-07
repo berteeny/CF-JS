f81b3106e5f5e63897790d033b546ad4f7943d1b
berteeny <aegbertoni@gmail.com>
2024-05-07 15:33:46 -0700

    Defined castList array with four objects, each including name, age and types variables

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,22 @@
-alert("Hello world");
-
-let favFood = "cherry cheesecake";
-document.write(favFood);
+let castList = [
+  {
+    name: "George",
+    age: 31,
+    types: ["bald", "unemployed"],
+  },
+  {
+    name: "Elaine",
+    age: 27,
+    types: ["loud", "curly"],
+  },
+  {
+    name: "Jerry",
+    age: 35,
+    types: ["funny", "single"],
+  },
+  {
+    name: "Kramer",
+    age: 39,
+    types: ["tall", "twitchy"],
+  },
+];
