4226c1101e850a327b08e7f0e45e9f68fc0bfcd3
berteeny <aegbertoni@gmail.com>
2024-05-10 11:52:14 -0700

    Wrapped castList array in IIFE and updated function outside

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,43 @@
-let castList = [
-  {
-    name: "George",
-    age: 31,
-    types: ["bald", "unemployed"],
-  },
-  {
-    name: "Elaine",
-    age: 27,
-    types: ["loud", "curly"],
-  },
-  {
-    name: "Jerry",
-    age: 35,
-    types: ["funny", "single"],
-  },
-  {
-    name: "Kramer",
-    age: 39,
-    types: ["tall", "twitchy"],
-  },
-];
+let characterRepository = (function () {
+  let castList = [
+    {
+      name: "George",
+      age: 31,
+      types: ["bald", "unemployed"],
+    },
+    {
+      name: "Elaine",
+      age: 27,
+      types: ["loud", "curly"],
+    },
+    {
+      name: "Jerry",
+      age: 35,
+      types: ["funny", "single"],
+    },
+    {
+      name: "Kramer",
+      age: 39,
+      types: ["tall", "twitchy"],
+    },
+  ];
 
-// writes character's name + age in document
-castList.forEach(function (character) {
+  function getAll() {
+    return castList;
+  }
+
+  function add(castMember) {
+    castList.push(castMember);
+  }
+
+  return {
+    getAll: getAll,
+    add: add,
+  };
+})();
+
+// writes charatcer name and age in document
+characterRepository.getAll().forEach(function (character) {
   document.write(
     `${character.name}${" (age "}${character.age}${")"}${"<br />"}`
   );
